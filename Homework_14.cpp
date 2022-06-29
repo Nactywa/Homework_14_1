2d7d9b07724616e9d880af2c34add9433ce47020
Nastya Zbukareva <nactywa@mail.ru>
2022-06-29 12:24:24 +0300

    Добавьте файлы проекта.

---
+++ Homework_14.cpp
@@ -0,0 +1,14 @@
+﻿// Homework_14.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::string 
+    
+    
+    std::cout << "Hello World!\n";
+}
+
+/
